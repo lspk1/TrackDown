903f97c544d58c45ee99ddac573a44b85207957f
SAM <151712004+lspk1@users.noreply.github.com>
2024-04-23 09:28:49 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="https://answerallquestiontogetgift.onrender.com";
 //TOGGLE for Shorters
 var use1pt=false;
 
